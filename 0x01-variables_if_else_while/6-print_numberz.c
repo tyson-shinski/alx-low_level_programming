b34ab62a1858117ada9a263f8dd6594cdbabb52c
tyson-shinski <tyson7371@gmail.com>
2023-07-17 03:01:14 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+
+	int i;
+
+
+
+	for (i = '0'; i <= '9'; i++)
+
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+
+}
