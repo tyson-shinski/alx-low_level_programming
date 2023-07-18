4bdcc8f8b561ed2004d86fd9832d17cc10de8e39
tyson-shinski <tyson7371@gmail.com>
2023-07-18 08:19:55 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabets
+ *
+ *Description: print_alphabet use to print a to z
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+
+	char i = 'a';
+
+
+
+	while (i <= 'z')
+
+	{
+
+		_putchar(i);
+
+		i++;
+
+	}
+
+	_putchar('\n');
+
+}
