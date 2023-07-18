77d7ee01c65a132e1bedfb9ddcb403a2ef37b8be
tyson-shinski <tyson7371@gmail.com>
2023-07-18 08:12:26 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *main - print out the characters
+ *
+ * Description: return the required results
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+
+{
+
+	int n = 0;
+
+	char name[8] = "_putchar";
+
+
+
+	while (n < 8)
+
+	{
+
+		_putchar(name[n]);
+
+		n++;
+
+	}
+
+	_putchar('\n');
+
+	return (0);
+
+}
