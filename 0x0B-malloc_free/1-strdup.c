8d27dad6b4853073ce957862653c87d8316d1c0e
Tyson <121870970+tyson-shinski@users.noreply.github.com>
2023-08-07 16:20:48 +0300

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * _strdup - returns a pointer to a newly allocated space in memory.
  * @str: string.
@@ -10,16 +11,21 @@ char *_strdup(char *str)
 {
     char *strout;
     unsigned int i, j;
+
     if (str == NULL)
         return (NULL);
+
     /* Calculate the length of the input string */
     for (i = 0; str[i] != '\0'; i++)
         continue;
+
     strout = (char *)malloc(sizeof(char) * (i + 1));
     if (strout == NULL)
         return (NULL);
+
     /* Copy the contents of the input string to the new memory location */
     for (j = 0; j <= i; j++)
         strout[j] = str[j];
+
     return (strout);
 }
