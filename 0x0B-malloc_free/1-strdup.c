348cfcd8dcd2e211433d38757a7b36d3a580b9ce
Tyson <121870970+tyson-shinski@users.noreply.github.com>
2023-08-07 16:22:03 +0300

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,23 +9,23 @@
  */
 char *_strdup(char *str)
 {
-    char *strout;
-    unsigned int i, j;
+	char *strout;
+	unsigned int i, j;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    /* Calculate the length of the input string */
-    for (i = 0; str[i] != '\0'; i++)
-        continue;
+	/* Calculate the length of the input string */
+	for (i = 0; str[i] != '\0'; i++)
+		continue;
 
-    strout = (char *)malloc(sizeof(char) * (i + 1));
-    if (strout == NULL)
-        return (NULL);
+	strout = (char *)malloc(sizeof(char) * (i + 1));
+	if (strout == NULL)
+		return (NULL);
 
-    /* Copy the contents of the input string to the new memory location */
-    for (j = 0; j <= i; j++)
-        strout[j] = str[j];
+	/* Copy the contents of the input string to the new memory location */
+	for (j = 0; j <= i; j++)
+		strout[j] = str[j];
 
-    return (strout);
+	return (strout);
 }
