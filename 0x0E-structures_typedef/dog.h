5150a60fabd119409d81f16bf88a34abae50f140
tyson-shinski <tyson7371@gmail.com>
2023-08-14 08:53:15 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
+
 /**
  * init_dog - initializes a variable of type struct dog.
  * @d: struct dog.
@@ -8,21 +11,12 @@
  *
  * Return: no return.
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
-
 {
-
-	if (d)
-
-	{
-
-		d->name = name;
-
-		d->age = age;
-
-		d->owner = owner;
-
-	}
-
+    if (d)
+    {
+        d->name = strdup(name); // Allocate memory and copy name
+        d->age = age;
+        d->owner = strdup(owner); // Allocate memory and copy owner
+    }
 }
