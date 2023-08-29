30f832b9058ab8602e50996bd9692bb38aff11c0
tyson-shinski <tyson7371@gmail.com>
2023-08-29 11:51:55 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements in a listint_t list.
+ * @h: A pointer to the head of the list.
+ *
+ * Return: The number of elements in the list.
+ */
+size_t listint_len(const listint_t *h)
+{
+    size_t count = 0;  // Initialize a count to keep track of the number of nodes
+
+    while (h != NULL)
+    {
+        count++;          // Increment the count for each node
+        h = h->next;      // Move to the next node
+    }
+
+    return count;  // Return the total count of nodes
+}
